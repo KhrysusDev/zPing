a1a1702a17d049e1bc749780d2d44d4a909c58aa
Khrysus <59972716+KhrysusDev@users.noreply.github.com>
2021-03-21 18:24:56 +0000

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -16,12 +16,6 @@ namespace zPing
      * KhrysusDev/zPing
      * Thursday 4th March 2021, 21:22 (UTC+0)
      * Licensed under Common Development and Distribution License (CDDL)
-     *
-     * Hi RavelCros!
-     * Enjoy this source code, because I know when you see this your 1000000% going to rip it
-     * Accept it as a gift from me, but this is here so companies can see some of the stuff I can do.
-     * And for all you kids that are probably going to rip it, just remember, ripping gets you nowhere in life.
-     * If you rip source, its highly unlikely that you will ever learn how to actually program.
      */
 
     internal class Program
